787336058eaaecca9b887c0911d647eceff6c06d
Zgrovetin <534477@gmail.com>
2025-02-18 13:12:53 +0100

    Initial commit

---
+++ database/migrations/2025_02_18_113314_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Category;
+use App\Models\Project;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignIdFor(Project::class);
+            $table->foreignIdFor(Category::class);
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('status');
+
+            $table->dateTime('due_date');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
