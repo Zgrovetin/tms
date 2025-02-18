0ca478e9b983608289baa63959bc392f96862ff2
Zgrovetin <534477@gmail.com>
2025-02-18 16:22:21 +0100

    cross version comp problems

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,15 +4,25 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
+use Illuminate\Support\Facades\Gate;
+//use App\Http\Traits\CanLoadRelationships;
 
 class ProjectController extends Controller
 {
+//    use CanLoadRelationships;
+
+    private array $relations = ['category', 'tasks','tasks.category'];
+
+
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        Gate::authorize('viewAny', Project::class);
+
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -10,10 +10,42 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+// Laravel 10
+//Route::apiResource('projects', ProjectController::class);
+//Route::apiResource('projects.tasks', TaskController::class)
+//    ->scoped(['task' => 'project']);
 
-Route::apiResource('projects', ProjectController::class);
-Route::apiResource('categories', CategoryController::class);
+//Route::apiResource('categories', CategoryController::class);
+//Route::apiResource('categories.tasks', TaskController::class)
+//    ->scoped(['task' => 'category']);
+
+// Laravel 11
+// public routes
+Route::apiResource('projects', ProjectController::class)
+    ->only(['index', 'show']);
+Route::apiResource('categories', CategoryController::class)
+    ->only(['index', 'show']);
+
 Route::apiResource('projects.tasks', TaskController::class)
-    ->scoped(['task' => 'project']);
+    ->scoped()
+    ->only(['index', 'show']);
 Route::apiResource('categories.tasks', TaskController::class)
-    ->scoped(['task' => 'category']);
+    ->scoped()
+    ->only(['index', 'show']);
+
+// protected routes
+Route::apiResource('projects', ProjectController::class)
+    ->only(['store', 'update', 'destroy'])
+    ->middleware(['auth:sanctum', 'throttle:api']);
+Route::apiResource('categories', CategoryController::class)
+    ->only(['store', 'update', 'destroy'])
+    ->middleware(['auth:sanctum', 'throttle:api']);
+
+Route::apiResource('projects.tasks', TaskController::class)
+    ->scoped()
+    ->only(['store', 'update', 'destroy'])
+    ->middleware(['auth:sanctum', 'throttle:api']);
+Route::apiResource('categories.tasks', TaskController::class)
+    ->scoped()
+    ->only(['store', 'update', 'destroy'])
+    ->middleware(['auth:sanctum', 'throttle:api']);
